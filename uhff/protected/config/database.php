1c42da5105ee71a53a7c49cec95ed82867bb615e
Carlos Ruiz <car.ruiz90@gmail.com>
2018-02-10 02:39:25 -0600

    cambio de permisos en ubuntu

--- uhff/protected/config/database.php
+++ uhff/protected/config/database.php
@@ -8,7 +8,7 @@ return array(
 	'connectionString' => 'mysql:host=localhost;dbname=uhff',
 	'emulatePrepare' => true,
 	'username' => 'root',
-	'password' => '',
+	'password' => 'root',
 	'charset' => 'utf8',
 	
 );
